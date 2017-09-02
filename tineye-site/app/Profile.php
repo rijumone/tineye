a0825c8ea2cd44e31e0eb4caa065a1e7a2fab8e8
Riju <mailmeonriju@gmail.com>
2017-09-03 00:19:11 +0530

    data migrate and fake

--- tineye-site/app/Profile.php
+++ tineye-site/app/Profile.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
-    //
+    protected $fillable = [
+    	'first_name',
+    	'last_name',
+    	'age',
+    	'sex',
+    	'active',
+    	'bio',
+    	'instagram',
+    	'spotify',
+    	'locality_id',
+    	'city_id',
+    	'state_id',
+    	'country_id',
+    ];
 }
--- tineye-site/routes/web.php
+++ tineye-site/routes/web.php
@@ -11,6 +11,30 @@
 |
 */
 
+// use App\Profile;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*
+Route::get('/seed', function (Profile $profile) {
+    $f = \Faker\Factory::create();
+    foreach(range(0,1000) as $x){
+    	$profile->create([
+    		'first_name' => $f->firstName(),
+    		'last_name' => $f->lastName(),
+    		'age' => $f->numberBetween($min = 18, $max = 100),
+    		'sex' => $f->randomElement(['man','woman']),
+    		'active' => $f->numberBetween($min = 0, $max = 1),
+    		'bio' => $f->paragraph($nbSentences = 3, $variableNbSentences = true),
+    		'instagram' => $f->url(),
+    		'spotify' => $f->url(),
+    		'locality_id' => $f->numberBetween($min = 1, $max = 10),
+    		'city_id' => $f->numberBetween($min = 1, $max = 10),
+    		'state_id' => $f->numberBetween($min = 1, $max = 7),
+    		'country_id' => $f->numberBetween($min = 1, $max = 5),
+    	]);
+    }
+});
+*/
