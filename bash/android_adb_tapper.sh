6f10678de6214c38793822396df4352726986a1d
Riju <mailmeonriju@gmail.com>
2017-09-03 03:10:45 +0530

    will stop taking screenshots if last screenshot has the same size (~same content)

--- bash/android_adb_tapper.sh
+++ bash/android_adb_tapper.sh
@@ -1,31 +1,75 @@
+# for i in $(seq 3 -1 1)
+# 	do
+# 		res=$((1 - i))
+# 		echo $res
+# 		if [ "$res" -lt 0 ]
+# 		then
+# 			res=$((res * -1))
+# 		fi
+# 		echo $res
+# 	done
 # adb shell getevent -l
-while true; do
+while true; do # forever
     # adb shell input tap 762 1663 				# like
     timestamp=$(date +%s)
-	mkdir data/$timestamp # mkdir with current time
+	mkdir /media/rijumone/3AB8-1F06/Download/tmp/data/$timestamp # mkdir with current time
 	echo $timestamp 
 	echo "entering details page"
     adb shell input tap 400 850 				# tap on image; enter details page
     sleep .7								# allow app to catch up before capturing screenshot
     echo "capturing main screenshot"
     adb shell screencap /sdcard/screen.png
-    adb pull /sdcard/screen.png data/$timestamp/main.png
+    adb pull /sdcard/screen.png /media/rijumone/3AB8-1F06/Download/tmp/data/$timestamp/main.png
 	for i in $(seq 1 1 3)
 	do
 		echo "initiating swipe for main page"
 		adb shell input swipe 400 900 400 300 200 # vertical swipe on image
 		echo "saving screenshot"
 		adb shell screencap /sdcard/screen.png
-    	adb pull /sdcard/screen.png data/$timestamp/"main_"$i.png
+    	adb pull /sdcard/screen.png /media/rijumone/3AB8-1F06/Download/tmp/data/$timestamp/"main_"$i.png
+		
+		# compare current screenshot to last captured screenshot
+		filesize_current=$(stat -c%s /media/rijumone/3AB8-1F06/Download/tmp/data/$timestamp/"main_"$i.png)
+		filesize_last=$(stat -c%s /media/rijumone/3AB8-1F06/Download/tmp/data/$timestamp/"main_"$((i - 1)).png)
+		difference=$((filesize_current - filesize_last))
+		# echo $difference
+		if [ "$difference" -lt 0 ]
+		then
+			difference=$((difference * -1))
+		fi
+		echo $difference
+		if [ "$difference" -lt 10 ] # difference in file size is less than 10 bytes
+		then
+			rm /media/rijumone/3AB8-1F06/Download/tmp/data/$timestamp/"main_"$i.png # delete current screenshot
+			break
+		fi
+		
 	done
 	adb shell input swipe 400 300 400 900 50 # vertical swipe on image to get it back to images
 	for i in $(seq 1 1 5)
 	do
 		echo "initiating swipe for images"
 		adb shell input swipe 800 850 100 850 100 # horizontal swipe on image
+		sleep .5	# allow app to catch up to ensure screenshot accuracy
 		echo "saving screenshot"
 		adb shell screencap /sdcard/screen.png
-    	adb pull /sdcard/screen.png data/$timestamp/$i.png
+    	adb pull /sdcard/screen.png /media/rijumone/3AB8-1F06/Download/tmp/data/$timestamp/$i.png
+
+    	# compare current screenshot to last captured screenshot
+		filesize_current=$(stat -c%s /media/rijumone/3AB8-1F06/Download/tmp/data/$timestamp/$i.png)
+		filesize_last=$(stat -c%s /media/rijumone/3AB8-1F06/Download/tmp/data/$timestamp/$((i - 1)).png)
+		difference=$((filesize_current - filesize_last))
+		# echo $difference
+		if [ "$difference" -lt 0 ]
+		then
+			difference=$((difference * -1))
+		fi
+		echo $difference
+		if [ "$difference" -lt 10 ] # difference in file size is less than 10 bytes
+		then
+			rm /media/rijumone/3AB8-1F06/Download/tmp/data/$timestamp/$i.png # delete current screenshot
+			break
+		fi
 	done
 	echo "exiting details page"
 	adb shell input tap 875 1650 				# tap on like; exit details page
