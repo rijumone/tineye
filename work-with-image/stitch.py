d40ad8b2571945a495cecd7fd86d40c72c291895
Riju <mailmeonriju@gmail.com>
2017-09-26 02:35:59 +0530

    calculating some data, let's see what happens

--- work-with-image/stitch.py
+++ work-with-image/stitch.py
@@ -2,13 +2,13 @@
 
 from PIL import Image
 import sys, time
-
+import os
 from scipy.misc import imread
 from scipy.linalg import norm
 from scipy import sum, average
-
+from shutil import copyfile
 def main(file1="",file2=""):
-    print time.time()
+    # print time.time()
     if file1 is "" and file2 is "":
         file1, file2 = sys.argv[1:1+2]
     # read images as 2D arrays (convert to grayscale for simplicity)
@@ -16,10 +16,10 @@ def main(file1="",file2=""):
     img2 = to_grayscale(imread(file2).astype(float))
     # compare
     n_m, n_0 = compare_images(img1, img2)
-    print "Manhattan norm:", n_m, "/ per pixel:", n_m/img1.size
-    print "Zero norm:", n_0, "/ per pixel:", n_0*1.0/img1.size
-    print time.time()
-    return n_0*1.0/img1.size
+    # print "Manhattan norm:", n_m, "/ per pixel:", n_m/img1.size
+    # print "Zero norm:", n_0, "/ per pixel:", n_0*1.0/img1.size
+    # print time.time()
+    return (n_m,n_m/img1.size,n_0,n_0*1.0/img1.size)
 
 def compare_images(img1, img2):
     # normalize to compensate for exposure difference
@@ -46,42 +46,75 @@ def normalize(arr):
 # if __name__ == "__main__":
 #     main()
 
-img = Image.open("main.png")
+for directory in os.listdir("/media/rijumone/Seagate BUP Slim1/bkp/data/Delhi"):
+    # print directory
 
-print "generating cropped image from main image bottom"
+    copyfile("/media/rijumone/Seagate BUP Slim1/bkp/data/Delhi/"+directory+"/main.png", "/home/rijumone/Kitchen/tineye/work-with-image/main.png")
+    copyfile("/media/rijumone/Seagate BUP Slim1/bkp/data/Delhi/"+directory+"/main_1.png", "/home/rijumone/Kitchen/tineye/work-with-image/main_1.png")
 
-width = img.size[0]
-height = img.size[1]
-img3 = img.crop(
-    (
-        0,
-        height - 800,
-        width,
-        height - 200
-    )
-)
-img3.save("main_cropped.png")
+    img = Image.open("main.png")
 
-zero_norm_pp = 1
-
-while zero_norm_pp > 0.04 :
-    # keep generating images and comparing
-    print "now generating cropped image from main image 1 top"
-
-    img = Image.open("main_1.png")
+    # print "generating cropped image from main image bottom"
 
     width = img.size[0]
     height = img.size[1]
     img3 = img.crop(
         (
             0,
-            height - 1210,
+            height - 789,
             width,
-            height - 610
+            height - 200
         )
     )
-    img3.save("main_cropped_1.png")
+    img3.save("main_cropped.png")
 
-    zero_norm_pp = main("main_cropped.png","main_cropped_1.png")
+    zero_norm_pp = 1
+    current_height = -1
+    ctr = 0
+    lowestVals = {"a":9999999999,"b":9999999999,"c":9999999999,"d":9999999999}
+    while True :
+    # while zero_norm_pp > 0.07 :
+        # keep generating images and comparing
+        # print "now generating cropped image from main image 1 top"
+        
+        current_height+=1
+        
+        img = Image.open("main_1.png")
+
+        width = img.size[0]
+        height = img.size[1]
+        img3 = img.crop(
+            (
+                0,
+                current_height,
+                width,
+                current_height + 589
+            )
+        )
+        img3.save("main_cropped_1.png")
+
+        returnVals = main("main_cropped.png","main_cropped_1.png")
+        if lowestVals["a"] > returnVals[0]:
+            lowestVals["a"] = returnVals[0]
+        if lowestVals["b"] > returnVals[1]:
+            lowestVals["b"] = returnVals[1]
+        if lowestVals["c"] > returnVals[2]:
+            lowestVals["c"] = returnVals[2]
+        if lowestVals["d"] > returnVals[3]:
+            lowestVals["d"] = returnVals[3]
+
+        ctr+=1
+        print returnVals
+        print lowestVals
+        if ctr > 1331: # image end reached
+            break
+
+    with open('data.csv','w+') as a:
+    # a.writelines("Name,Age\n")
+        a.writelines(lowestVals["a"] + ","  + lowestVals["b"] + ","  + lowestVals["c"] + ","  + lowestVals["d"] + "\n") 
 
 # 0.0384521604938
+
+# {'a': 491480.83431952458, 'c': 396102.0, 'b': 0.77262282952827233, 'd': 0.62268439916996798} Renuka
+# {'a': 131340.75842696629, 'c': 6459.0, 'b': 0.20647166953871329, 'd': 0.010153744576495} Sharon
+# {'a': 11003.5, 'c': 1100.0, 'b': 0.017297836886122114, 'd': 0.0017292334779601332} Pankhuri
